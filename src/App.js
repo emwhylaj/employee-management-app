d9c84fa66fa33df3537c1147bfc07c76d3584a21
Muyiwa Olalekan <muyiwa.olalekan@thebulb.africa>
2021-02-07 12:45:09 +0100

    Uodated work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Button from "react-bootstrap/Button";
 import { Home } from "./components/Home";
 import { Employee } from "./components/Employee";
 import { Department } from "./components/Department";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="mt-5 d-flex justify-content-left">
+      <div className="container">
+        <h3 className="m-3 d-flex justify-content-center">
+          React JS with web API demo
+        </h3>
+        <h5 className="m-3 d-flex justify-content-center">
+          Employee Management Portal
+        </h5>
+        <Navigation />
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/" component={Employee} />
--- src/components/Department.jsx
+++ src/components/Department.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
+import { Table } from "react-boostrap";
 
 export class Department extends Component {
+  constructor(prop) {
+    super(prop);
+    this.state = { deps: [] };
+  }
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList() {
+    this.setState({
+      deps: [
+        { DepartmentID: 1, Department: "IT" },
+        { DepartmentID: 2, DepartmentName: "Account" },
+      ],
+    });
+  }
   render() {
     return (
-      <div>
-        <h3>I am the department page</h3>
-      </div>
+      <Table className="mt-4" striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>DepartmentID</th>
+            <th>DepartmentName</th>
+          </tr>
+        </thead>
+        <tbody>
+          {deps.map((dep) => (
+            <tr key={dep.DepartmentID}>
+              <td>{dep.DepartmentID}</td>
+            </tr>
+          ))}
+          ;
+        </tbody>
+      </Table>
     );
   }
 }
